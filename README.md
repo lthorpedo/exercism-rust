19e2a6a670769276150b45bddf8c19cbf1d58c79
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-06 10:00:06 -0700

    add readme, wip on luhn validator

---
+++ README.md
@@ -0,0 +1,12 @@
+# Learning Rust in Exercism
+
+I'm learning [rust](https://www.rust-lang.org/) for fun, on [exercism](https://exercism.org/tracks/rust).
+
+
+### Exercism Profile
+
+See my [profile](https://exercism.org/profiles/lthorpedo) on exercism.
+
+### Why rust?
+
+Rust is a popular, up and coming language. It is performant, memory safe, strongly typed, and known to teach programmers to "do things right."
--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,55 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    // luhn codes that are of length 1 or less are invalid
+    if code.len() < 2 {
+        return false;
+    }
+
+    const RADIX: u32 = 10;
+
+    let dig = ' ';
+    let my_bool = dig.is_ascii_digit();
+    todo!("dig {dig} {my_bool}");
+
+    let mut digits = Vec::new();
+    let mut digits2 = Vec::new();
+    // loop over each digit (reversed / right to left)
+    code.chars()
+        .rev()
+        .filter(|c| c.is_ascii_digit())
+        .map(|c_dig| digits.push(c_dig.to_digit(RADIX).unwrap() as i32));
+
+    // double every second digit, and if it's greater than 9 then subtract 9
+    for (index, n) in digits.iter().enumerate() {
+        let mut temp = *n;
+        if index % 2 == 1 {
+            temp = n * 2;
+            if temp > 9 {
+                temp = temp - 9;
+            }
+        }
+        digits2.push(temp);
+    }
+
+    let sum = digits2.into_iter()
+        .reduce(|a, b| a + b)
+        .unwrap();
+
+    todo!("what's teh sum {sum}");
+
+    return sum % 10 == 0;
 }
+
+
+
+
+
+
+// add to settings.json to not edit rust libraries :)
+// 
+// "files.readonlyInclude": {
+//   "**/.cargo/registry/src/**/*.rs": true,
+//   "**/.cargo/git/checkouts/**/*.rs": true,
+//   "**/lib/rustlib/src/rust/library/**/*.rs": true,
+// },
+//
