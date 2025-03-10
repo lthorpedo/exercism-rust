b8b8257cb4bddd52c78909209c3547fc35dfdb37
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-10 10:29:01 -0600

    cleanup armstrong

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -6,12 +6,8 @@ pub fn is_armstrong_number(num: u32) -> bool {
     
     // loop and reduce to the sum
     let total = str.chars()
-        .filter(|c| c.is_ascii_digit())
         .map(|c| c.to_digit(10).unwrap())
         .fold(0, |sum, digit| sum + digit.pow(power));
-
-    // dbg!(total);
-    // dbg!(power);
     
     num == total
 }
