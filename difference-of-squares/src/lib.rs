3758ab3927451511e96575fe66609bf949eeca07
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-12 18:43:08 -0600

    eulers project #6 :)

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,11 +1,16 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    let sum = (1..=n)
+        .reduce(|acc, x| acc + x)
+        .unwrap();
+    sum.pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    (1..=n)
+        .reduce(|acc, x| acc + x.pow(2))
+        .unwrap()
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
