4d3e19b9849a2c644e42da417105fe478cdd9db3
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-19 16:34:57 -0600

    functional beauty

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -2,12 +2,11 @@ use regex::Regex;
 
 pub fn abbreviate(phrase: &str) -> String {
     let splitter = Regex::new(r"([-\s]+)").unwrap();
-    let x: Vec<String> = splitter.split(phrase)
+    splitter.split(phrase)
         .filter(|s| s.chars().filter(|c| c.is_alphabetic()).count() > 0)
         .map(|word| String::from(
             word.chars().find(|letter| letter.is_alphabetic()).unwrap().to_ascii_uppercase()
         ))
-        .collect();
-
-    x.join("")
+        .collect::<Vec<String>>()
+        .join("")
 }
