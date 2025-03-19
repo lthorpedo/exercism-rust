b8cfc076fc364783e10b66a38cc99162253b45ca
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-19 16:30:25 -0600

    added regex to acronym. split based on whitespace and dashes.

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,5 +1,13 @@
+use regex::Regex;
+
 pub fn abbreviate(phrase: &str) -> String {
-    Regex::new(r"([-\s]+)").split(phrase)
+    let splitter = Regex::new(r"([-\s]+)").unwrap();
+    let x: Vec<String> = splitter.split(phrase)
         .filter(|s| s.chars().filter(|c| c.is_alphabetic()).count() > 0)
-        .map()
+        .map(|word| String::from(
+            word.chars().find(|letter| letter.is_alphabetic()).unwrap().to_ascii_uppercase()
+        ))
+        .collect();
+
+    x.join("")
 }
