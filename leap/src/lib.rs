846635983e78938072a77518ee516731df064c24
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-18 21:16:28 -0600

    leap year easy peasy

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    if year % 4 == 0 {
+        if year % 400 == 0 {
+            return true;
+        }
+        if year % 100 == 0 {
+            return false;
+        }
+        return true;
+    }
+    false
 }
