e08979f6618c4061abf28414963e941bdd454923
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-18 21:19:50 -0600

    match statement for fun

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,12 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 {
-        if year % 400 == 0 {
-            return true;
-        }
-        if year % 100 == 0 {
-            return false;
-        }
-        return true;
+    match year {
+        x if x % 400 == 0 => true,
+        x if x % 100 == 0 => false,
+        x if x % 4 == 0 => true,
+        _ => false,
     }
-    false
 }
