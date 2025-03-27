d9815aa162045cfb90bd4103b798093ecd5a42c8
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-27 11:26:43 -0600

    calculating the nth prime number (0 based indexing - so the first prime number 2 is the 0th prime num)

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    let mut counter = n as i32;
+    let mut idx = 1_u32;
+
+    while counter > -1 {
+        idx += 1;
+        if is_prime(idx) { counter -= 1; }
+    }
+
+    idx
+}
+
+fn is_prime(num: u32) -> bool {
+    if num < 2 { return false; }
+    if num == 2 { return true; }
+
+    let max = f64::sqrt(num as f64) as u32 + 1;
+
+    for i in 2..=max {
+        if num % i == 0 { return false; }
+    }
+
+    true
 }
