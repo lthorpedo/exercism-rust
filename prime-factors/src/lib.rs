7132f4dd999d726cfb39fe0e61acac4f97477c59
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-27 17:11:49 -0600

    updated based on optimizations found with help

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,35 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut ret= Vec::new();
-    let max = (n / 2) as u64 + 1;
     let mut num = n;
-    let mut idx = 2_u64;
+    let mut temp = 2_u64;
 
-    while !is_prime(num) && idx < max {
-        if num % idx == 0 {
-            num = num / idx;
-            ret.push(idx);
-            idx = 1;
+    while num > 1 {
+        while num % temp == 0 {
+            num /= temp;
+            ret.push(temp);
         }
-        idx += 1;
-    }
-
-    if is_prime(num) {
-        ret.push(num);
+        temp += 1;
     }
 
     ret
-}
-
-
-fn is_prime(num: u64) -> bool {
-    if num < 2 { return false; }
-    if num == 2 { return true; }
-
-    let max = f64::sqrt(num as f64) as u64 + 1;
-
-    for i in 2..=max {
-        if num % i == 0 { return false; }
-    }
-
-    true
 }
