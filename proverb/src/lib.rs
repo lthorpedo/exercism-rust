2219ce1b4b1a48e970b41c048e47625e45c38529
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-27 19:35:30 -0600

    Optimize proverbs

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,18 +1,12 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let first = match list.first() {
-        Some(n) => n,
-        None => return String::from("")
-    };
-
-    let mut msg = list.iter()
-        .zip(list.iter().skip(1))
-        // .filter(|(a,b)|)
-        .map(|(a, b)| format!("For want of a {a} the {b} was lost."))
-        .collect::<Vec<String>>()
-        .join("\n");
-    if !msg.is_empty() {
-        msg += "\n";
+    match list.first() {
+        None => String::new(),
+        Some(word) => list.iter()
+            .zip(list.iter().skip(1))
+            .map(|(a, b)| format!("For want of a {a} the {b} was lost.\n"))
+            .chain(once(format!("And all for the want of a {word}.")))
+            .collect(),
     }
-
-    format!("{msg}And all for the want of a {first}.")
 }
