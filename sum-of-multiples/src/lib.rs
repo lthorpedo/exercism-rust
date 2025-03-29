f687f212887390f5321d13c3e3551f8533a0f882
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2025-03-29 12:49:08 -0600

    solved sum of multiples :)

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut integers = HashSet::new();
+
+    for i in factors.iter().filter(|x| **x != 0) {
+        let mut idx = 1_u32;
+        while idx < limit {
+            if idx % i == 0 && !integers.contains(&idx) {
+                integers.insert(idx);
+            }
+            idx += 1;
+        }
+    }
+
+    integers.iter().sum()
 }
